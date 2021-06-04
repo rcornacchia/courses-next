d61166889cddd068fdda73f64b4f569ae4fe3245
Rob Cornacchia <rlc2160@columbia.edu>
2021-06-03 23:27:13 -0400

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+// import CourseList from "components/CourseList";
+import { getCourses } from "../utils/db";
+
+const Homepage = ({ courses }) => {
+  return (
+    <div>
+      <h1>Courses</h1>
+      <pre>{JSON.stringify(courses, null, 2)}</pre>
+    </div>
+  );
+};
+
+export const getStaticProps = async () => {
+  const data = await getCourses();
+
+  return {
+    props: {
+      courses: JSON.parse(JSON.stringify(data)),
+    },
+  };
+};
+
+export default Homepage;
